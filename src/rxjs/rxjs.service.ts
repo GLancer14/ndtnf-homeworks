ef68841dfe4996e4c28d9071c523e07a4c21426b
unknown <181259602+GLancer14@users.noreply.github.com>
2025-11-06 18:01:26 +0400

    Task 009-rxjs solved

--- src/rxjs/rxjs.service.ts
+++ src/rxjs/rxjs.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from "@nestjs/common";
+import { BadRequestException, Injectable } from "@nestjs/common";
 import {
   firstValueFrom,
   toArray,
@@ -13,6 +13,7 @@ import axios from "axios";
 @Injectable()
 export class RxjsService {
   private readonly githubURL = "https://api.github.com/search/repositories?q=";
+  private readonly gitLabURL = "https://gitlab.com/api/v4/projects?search=";
 
   private getGithub(text: string, count: number): Observable<any> {
     return from(axios.get(`${this.githubURL}${text}`))
@@ -23,10 +24,26 @@ export class RxjsService {
       .pipe(take(count));
   }
 
+  private getGitLab(text: string, count: number): Observable<any> {
+    return from(axios.get(`${this.gitLabURL}${text}`))
+      .pipe(
+        map((res: any) => res.data),
+        mergeAll(),
+      )
+      .pipe(take(count));
+  }
+
   async searchRepositories(text: string, hub: string): Promise<any> {
-    // Здесь можно добавить логику проверки на какой hub делать запрос
+    let data$: Observable<any>;
+    if (hub === "github") {
+      data$ = this.getGithub(text, 5).pipe(toArray());
+    } else if (hub === "gitlab") {
+      data$ = this.getGitLab(text, 5).pipe(toArray());
+    } else {
+      return new BadRequestException("Unknown hub");
+    }
+
     console.log("hub = ", hub);
-    const data$ = this.getGithub(text, 10).pipe(toArray());
     data$.subscribe(() => {});
     return await firstValueFrom(data$);
   }
